42dacf94f6419a819d78eafaeefaeed4167f4335
Daniel Ferreira <djf989@gmail.com>
2018-07-05 15:45:17 -0300

    added momentjs

--- src/components/Chat/MessageList/Message/Message.js
+++ src/components/Chat/MessageList/Message/Message.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 import './Message.css';
 
@@ -12,11 +13,29 @@ const propTypes = {
     }).isRequired
 }
 
+const formatDate = date => {
+
+    const mDate = moment(date);
+    const mTime = mDate.format('HH:mm');
+    const today = moment();
+    const yesterday = moment().add(-1, 'days');
+
+    if(mDate.isSame(today, 'day')) {
+        return mTime;
+    }
+
+    if(mDate.isSame(yesterday, 'day')) {
+        return `yesterday at ${mTime}`;
+    }
+
+    return `${mDate.format('MM/DD/YYYY')} at ${mTime}`;
+}
+
 const Message = ({ message }) =>
     <div className="message">
         <div className={classNames("message-bubble", "message-" + message.type)}>
             <p>{message.content}</p>
-            {/* <span>{message.date}</span> */}
+            <span>{formatDate(message.date)}</span>
         </div>
     </div>
 
