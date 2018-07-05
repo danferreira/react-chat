4c28f999b4f9d13b2b58284d751bcef8e427645a
Daniel Ferreira <djf989@gmail.com>
2018-07-05 15:41:53 -0300

    convert to object spread

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -4,19 +4,20 @@ import initialState from './initialState';
 export default function contactReducer(state = initialState.contacts, action) {
     switch (action.type) {
         case types.CONTACT_ADD:
-            var filteredList = state.list.filter(c => c.id !== action.contact.id);
-            return Object.assign({}, state, { list: filteredList.concat(action.contact) });
+            let filteredList = state.list.filter(c => c.id !== action.contact.id);
+
+            return { ...state, list: filteredList.concat(action.contact) }
         case types.CONTACT_UPDATE:
-            var list = state.list.map(c => {
+            let list = state.list.map(c => {
                 if (c.id === action.contact.id) {
                     c = action.contact
                 }
                 return c;
             });
 
-            return Object.assign({}, state, { list });
+            return { ...state, list }
         case types.SET_CURRENT_CONTACT:
-            return Object.assign({}, state, { current: action.contactId })
+            return { ...state, current: action.contactId }
         default:
             return state;
     }
