74f793c35bb35ccad48e339592509d3f5cc606d2
Daniel Ferreira <djf989@gmail.com>
2018-07-05 15:42:53 -0300

    added config

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCagyrM2VIH1cg1L20h7-Rzsqr3Z-llxdc",
+    authDomain: "react-chat-dan.firebaseapp.com",
+    databaseURL: "https://react-chat-dan.firebaseio.com",
+    projectId: "react-chat-dan",
+    storageBucket: "react-chat-dan.appspot.com",
+    messagingSenderId: "141126711035"
+  };
