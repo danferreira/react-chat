c1afd7334effbe782389d7ca378add8140b9d18e
Daniel Ferreira <djf989@gmail.com>
2018-07-05 15:42:35 -0300

    removed register page

--- src/pages/RegisterPage/RegisterPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import RegisterContainer from '../../containers/RegisterContainer';
-import './RegisterPage.css';
-
-const RegisterPage = () => <RegisterContainer />
-
-export default RegisterPage;
