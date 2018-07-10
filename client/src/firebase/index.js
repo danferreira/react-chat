286b1fdc038178cdc90549bc96ab36f4297f96a7
Daniel Ferreira <djf989@gmail.com>
2018-07-10 03:34:25 -0300

    removed firebase and redux-form

--- client/src/firebase/index.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase';
-import config from './config'
-
-firebase.initializeApp(config);
-
-export const auth = firebase.auth();
-export const database = firebase.database();
-
-export default firebase;
