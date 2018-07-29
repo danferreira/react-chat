85181142e8706a62f2f68abc396b6cb698fa674a
Daniel Ferreira <djf989@gmail.com>
2018-07-29 00:07:20 -0300

    format error message

--- client/src/containers/UserListContainer.js
+++ client/src/containers/UserListContainer.js
@@ -2,26 +2,33 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import gql from "graphql-tag";
 import styled from 'styled-components';
+import Spinner from '../components/Spinner/Spinner'
 
 import ProfileCard from '../components/ProfileCard/ProfileCard';
 
 const UserList = styled.div`
-display: flex;
-flex-flow: row wrap;
-margin-top: 200px;
+    display: flex;
+    flex-flow: row wrap;
+    margin-top: 50px;
+`;
+
+const Error = styled.div`
+    color: red;
+    font-size: 20px;
+    margin:20px;
 `;
 
 const UserListContainer = () => {
 
     return (
         <Query query={GET_ALL_USERS}>
-            {({ loading, error, data: { users } }) => {
-                if (loading) return "Loading...";
-                if (error) return `Error! ${error.message}`;
+            {({ loading, error, data }) => {
+                if (loading) return <Spinner />
+                if (error) return <Error>{`Error! ${error.message}`}</Error>;
 
                 return (
                     <UserList>
-                        {users.map(c => <ProfileCard contact={c} key={c.id}/>)}
+                        {data.users.map(c => <ProfileCard contact={c} key={c.id}/>)}
                     </UserList>
                 )
             }}
